58d5a2da7e7e0a81f526bb97720c063a09ab3a40
vinicius123131 <vini989073599@gmail.com>
2023-09-07 20:35:34 -0300

    feat: atualizando para o modelo

--- content_fuc.py
+++ content_fuc.py
@@ -7,17 +7,22 @@ def make_package(path, file_name):
 
 def fuc_content_case(path, file_name):
     packeage = make_package(path, file_name)
+    packeage_not_use_case = remove_after_string(packeage, "use_cases")
     file_name_pascal_case = to_pascal_case(file_name)
-    conteudo = f"""
-package {packeage};
+    conteudo = f"""package {packeage};
 
+import {packeage_not_use_case}.loggers.Logger;
+import {packeage}.metadata.UseCaseMetadata;
 import {packeage}.io.inputs.{file_name_pascal_case}UseCaseInput;
 import {packeage}.io.outputs.{file_name_pascal_case}UseCaseOutput;
 import {packeage}.specifics.functions.FunctionUseCase;
 
 public abstract class {file_name_pascal_case}UseCase extends FunctionUseCase<{file_name_pascal_case}UseCaseInput, {file_name_pascal_case}UseCaseOutput> {{
-    protected {file_name_pascal_case}UseCase() {{
-        super();
+    protected {file_name_pascal_case}UseCase(Logger logger) {{
+        super(
+                UseCaseMetadata.ofOpenAccessUseCase({file_name_pascal_case}UseCase.class, "some description about this use case"),
+                logger
+        );
     }}
 }}
     """
@@ -28,33 +33,28 @@ public abstract class {file_name_pascal_case}UseCase extends FunctionUseCase<{fi
 def fuc_content_factory(path, file_name):
     packeage = make_package(path, file_name)
     file_name_pascal_case = to_pascal_case(file_name)
-    conteudo = f"""
-package {packeage}.factories;
+    conteudo = f"""package {packeage}.factories;
 
-import {packeage}.CadastrarProdutoUseCase;
-import {packeage}.factories.dependency_wrapper.CadastrarProdutoUseCaseDependencyWrapper;
-import {packeage}.implementations.CadastrarProdutoUseCaseImplementation;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import {packeage}.{file_name_pascal_case};
+import {packeage}.factories.dependency_wrapper.{file_name_pascal_case}UseCaseDependencyWrapper;
+import {packeage}.implementations.{file_name_pascal_case}UseCaseImplementation;
 
 import java.util.Optional;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class {file_name_pascal_case}UseCaseFactory {{
 
-    private static {file_name_pascal_case}UseCase useCase = null;
+    private {file_name_pascal_case}UseCaseFactory(){{}}
+    
+    private static {file_name_pascal_case}UseCase singleton = null;
 
-    public static {file_name_pascal_case}UseCase generic({file_name_pascal_case}UseCaseDependencyWrapper dependencyWrapper){{
-        if (Optional.ofNullable(useCase).isPresent())
-            return useCase;
-        useCase = makeInstanceWith(dependencyWrapper);
-        return useCase;
+    public static {file_name_pascal_case}UseCase makeInstance({file_name_pascal_case}UseCaseDependencyWrapper dependencyWrapper){{
+        return Optional.ofNullable(singleton).orelseGet(() -> {{
+            singleton = new {file_name_pascal_case}UseCaseImplementation(dependencyWrapper.getLogger());
+            return singleton;
+        }});
     }}
-
-    private static {file_name_pascal_case}UseCase makeInstanceWith({file_name_pascal_case}UseCaseDependencyWrapper dependencyWrapper) {{
-        return new {file_name_pascal_case}UseCaseImplementation(
-        );
-    }}
+    
+    public static Optional<{file_name_pascal_case}> getSingleton() {{ return Optional.ofNullable(singleton);}}
 
 }}"""
     return conteudo
@@ -64,19 +64,27 @@ def fuc_content_wrapper(path, file_name):
     packeage = make_package(path, file_name)
     file_name_pascal_case = to_pascal_case(file_name)
     file_name_camel_case = to_camel_case(file_name)
+    packeage_not_use_case = remove_after_string(packeage, "use_cases")
 
-    conteudo = f"""
-package {packeage}.factories.dependency_wrapper;
-
-import br.com.stockio.use_cases.dependency_wrappers.UseCaseDependencyWrapper;
-import {packeage}.implementations.ports.{file_name_pascal_case}Port;
-import lombok.Builder;
-
-@Builder
-public class {file_name_pascal_case}UseCaseDependencyWrapper extends UseCaseDependencyWrapper {{
-
-    private final {file_name_pascal_case}Port {file_name_camel_case}Port;
-    private final {file_name_pascal_case}Factory {file_name_camel_case}Factory;
-
-}}"""
+    conteudo = f"""package {packeage}.factories.dependency_wrapper;
+    
+import {packeage}.factories.dependency_wrapper;
+    
+import {packeage_not_use_case}.Loggers.loggers;
+import {packeage}.dependency_wrappers.UseCaseDependencyWrapper;
+    
+    public class {file_name_pascal_case}UseCaseDependencyWrapper extends UseCaseDependencyWrapper {{
+    
+    //declare here the dependencies your uses case has
+    
+    private final Logger logger;
+    
+    public {file_name_pascal_case}UseCaseDependencyWrapper(Logger logger) {{this.logger = logger;}}
+    
+    //it is a good thing to make sure the attributes aren´t null, unless explicitly intended otherwise
+    
+    public Logger getLogger() {{return this.getValueWithNullSafety(this.logger);}}
+    
+    }}
+"""
     return conteudo
