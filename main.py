718e7b7548ea9c9b7ce53e733e9e2ce3a96fd8e7
vinicius123131 <vini989073599@gmail.com>
2023-11-04 17:40:13 -0300

    feat: melhorando a opcao de adicionar pelo terminal

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from db import *
 from criateFilesAndFolders import create_folder_structure, create_file_structure
 from template_commands_json import new_file_json, new_function_json
 from variables import ignore_the_first_arg, arg_index_of_use_case_name, arg_function_index, function_index
+import subprocess
+import tempfile
 
 
 def main():
@@ -27,35 +29,67 @@ def new(args):
 
 
 def add(args):
-    names = []
-    names.append(args[1])
-    dirs = []
-    files = []
-    print(f"Você está preste a criar um novo template '{args[1]}'")
-    while if_yes("do you want add a new name for this template?"):
-        name = input("new name: ")
-        names.append(name)
-    while if_yes("do you want add a new dir for create in this template?"):
-        dir = input("new dir: ")
-        dirs.append(dir)
-    while if_yes("do you want add a new file for create in this template?"):
-        name = input("name of file: ")
-        path = input("path relative of file: ")
-        content = input("content of file: ")
-        files.append(new_file_json(name, path, content))
+    names = [args[1]]
+    print(f"You are about to create a new template '{args[1]}'")
+    print(f"press only ENTER to exit")
+    names = name_add(names)
+    dirs = dir_add()
+    files = file_add()
     template_json = new_function_json(names, dirs, files)
     save_new_function(template_json, names[0])
 
 
-def if_yes(question):
+def name_add(names):
+    print(" ")
+    print("ID name for this templates")
     while True:
-        p = input(f"{question} y/n ")
-        if p.lower() == 'y':
-            return True
-        if p.lower() == 'n':
-            return False
-        else:
-            print("not valid, options: y our n")
+        name = input("name: ")
+        if name == "":
+            break
+        names.append(name)
+    return names
+
+
+def dir_add():
+    dirs = []
+    print(" ")
+    print("Dir to be created in this template: ")
+    while True:
+        dir = input("dir ")
+        if dir == "":
+            break
+        dirs.append(dir)
+    return dirs
+
+
+def file_add():
+    files = []
+    print(" ")
+    print("Files to be created in this template: ")
+    while True:
+        name = input("name of file: ")
+        if name == "":
+            break
+        path = input("path relative of file: ")
+        input("Press any to open editor for content template")
+        content = open_in_nano()
+        files.append(new_file_json(name, path, content))
+    return files
+
+
+def open_in_nano():
+    with tempfile.NamedTemporaryFile(suffix=".txt", delete=False) as temp_file:
+        temp_file.write(b"")
+        temp_file_name = temp_file.name
+    try:
+        subprocess.call(["notepad", temp_file_name])
+    except FileNotFoundError:
+        print("Please install nano or use a different text editor.")
+        return None
+    with open(temp_file_name, "r") as edited_file:
+        edited_content = edited_file.read()
+
+    return edited_content
 
 
 options_functions = {
