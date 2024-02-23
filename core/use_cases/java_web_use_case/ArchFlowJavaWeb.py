b2a0696dad6687e8d1231605a06e4de664901253
vinicius123131 <vini989073599@gmail.com>
2024-02-23 03:55:48 -0300

    feat: melhorias no cae com a implementação do archflow

--- core/use_cases/java_web_use_case/ArchFlowJavaWeb.py
+++ core/use_cases/java_web_use_case/ArchFlowJavaWeb.py
@@ -11,14 +11,15 @@ class ArchFlowJavaWeb(ArchFlow):
     def create_project(self, group_id, artifact_id, version, package_name, dependencies=None):
         maven_command = [
             "mvn",
-            "selfetype:generate",
+            "archetype:generate",  # Corrected the typo here
             "-DgroupId=" + group_id,
             "-DartifactId=" + artifact_id,
             "-Dversion=" + version,
             "-Dpackage=" + package_name,
-            "-DselfetypeArtifactId=maven-selfetype-quickstart",
+            "-DarchetypeArtifactId=maven-archetype-quickstart",  # Corrected the prefix here
             "-DinteractiveMode=false"
         ]
+
         try:
             self.OutputHandler.information_message("starting to create a java project using maven")
             process = subprocess.Popen(" ".join(maven_command), shell=True, stdout=subprocess.PIPE,
@@ -57,16 +58,41 @@ class ArchFlowJavaWeb(ArchFlow):
     def handle_args():
         args_input = sys.argv
         return args_input[1:]
-    
-    @staticmethod
-    def read_json_file(path_relative):
-        root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), path_relative))
-        
-        with open(root_path, 'r') as file_json:
-            content = json.load(file_json)
+
+    def read_json_file(self, path_relative):
+        try:
+            root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), path_relative))
+
+            with open(root_path, 'r') as file_json:
+                content = json.load(file_json)
+        except Exception as e:
+            self.OutputHandler.alert_message(f"Error reading file {path_relative} error: {e}")
         return content
 
     def handler_input(self, args):
+        def execute_step(steps_function, args_):
+            for dic in steps_function:
+                for step in dic:
+                    args_function = self.find_key_in_dictionaries(dic, step)
+                    function = self.find_key_in_dictionaries(dictonary_functions, step)
+                    if function is None:
+                        function = self.find_key_in_dictionaries(functions_json, step)
+                        steps_funcao = self.find_key_in_dictionaries(function, 'steps')
+                        args_function_ = self.find_key_in_dictionaries(dic, step)
+                        args_mapeados = self.map_args(args_function_, args_[0:], "tem[")
+                        execute_step(steps_funcao, args_mapeados)
+                        break
+                    try:
+                        if args_function == "None":
+                            function()
+                        elif isinstance(args_function, list):
+                            args_mapeados = self.map_args(args_function, args_[0:])
+                            function(*args_mapeados)
+                        else:
+                            function(*args_[1:])
+                    except TypeError as e:
+                        self.OutputHandler.alert_message(f"Error calling the function: {e}")
+
         if len(args) == 0:
             self.OutputHandler.success_message("Whoopsie-daisy! It seems like you forgot to provide a function. "
                                                "How about trying --help for some magic commands?")
@@ -79,29 +105,16 @@ class ArchFlowJavaWeb(ArchFlow):
         if func is not None:
             steps_funcao = self.find_key_in_dictionaries(func, 'steps')
             dictonary_functions = self.dictionary_of_standard_functions()
-            for dic in steps_funcao:
-                for step in dic:
-                    args_function = self.find_key_in_dictionaries(dic, step)
-                    function = self.find_key_in_dictionaries(dictonary_functions, step)
-                    try:
-                        if args_function == "None":
-                            function()
-                        elif isinstance(args_function, list):
-                            args_mapeados = self.map_args(args_function, args[1:])
-                            function(*args_mapeados)
-                        else:
-                            function(*args[1:])
-                    except TypeError as e:
-                        self.OutputHandler.alert_message(f"Error calling the function: {e}")
+            execute_step(steps_funcao, args)
         else:
             self.OutputHandler.alert_message(f"function '{nome_funcao}' is not valid, try another one or try --help ")
 
-    def map_args(self, list_args, list_replace):
+    def map_args(self, list_args, list_replace, parameter="args["):
         args_replace = []
         args_and_tags_replace = []
         for obj in list_args:
             for i, arg in enumerate(list_replace):
-                obj = obj.replace(f"args[{i}]", arg)
+                obj = obj.replace(f"{parameter}{i}]", arg)
             args_replace.append(obj)
         for tag in args_replace:
             tag_replace = self.StringManipulator.replace_tags(tag)
